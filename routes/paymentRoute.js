1710c608a40df863d3d83863209b0ec976565c8c
Pitacd <jappita15@hotmail.com>
2023-12-16 19:13:51 +0000

    Change route to pass payment

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const paymentController = require('../controllers/paymentController'); 
 
-router.get('/payment', paymentController.get);
+router.get('/pass/payment', paymentController.get);
 
 module.exports = router;
