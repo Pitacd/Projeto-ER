c4098bf28ad0176989bae5b8d30130c62d1729f6
Pitacd <jappita15@hotmail.com>
2023-12-17 19:09:17 +0000

    Add pageName

--- controllers/passVisualizationController.js
+++ controllers/passVisualizationController.js
@@ -2,7 +2,7 @@ const { getPassByOwner } = require('../models/passes');
 
 function get(req, res){
     pass = getPassByOwner('120');
-    res.render('viewPass.ejs', { pass });
+    res.render('viewPass.ejs', { pageTitle : "Passe", pass });
 }
 
 module.exports = { get };
