7fca4dbcdd43628722248ae14e92a3c0ef7f4e34
Pedro <jpcoutinhoalves@gmail.com>
2023-12-18 11:27:34 +0000

    Improved addPass function
    Added JSON to hold title types and price information

--- controllers/passAssociationController.js
+++ controllers/passAssociationController.js
@@ -1,12 +1,15 @@
 const { addPass } = require("../models/passes");
 
 function get(req, res){
-    res.render('connectPass.ejs', {pageTitle : "Passe"});
+    res.render('connectPass.ejs', { pageTitle : "Passe" });
 }
 
 function post(req, res){
+    // TODO use cookies to get the userId //
+    let userId = 1;
+    ////////////////////////////////////////
     let { busPassNumber, fullName } = req.body;
-    addPass(busPassNumber, fullName);
+    addPass(busPassNumber, userId, fullName);
     res.redirect('/pass');
 }
 
--- models/passes.js
+++ models/passes.js
@@ -1,33 +1,46 @@
+// Libraries
 const path = require('path');
 const fs = require('fs');
+const { randomInt } = require('crypto');
 
-function getPassByOwner(owner){
+// Constants
+const PAYMENT_STATES = ['Em atraso', 'Pago'];
+
+// Util functions
+const randomItem = (array) => { return array[randomInt(array.length)] ;}
+
+function getPassByOwner(ownerId){
     let buffer = fs.readFileSync(path.join(__dirname, 'passes.json'),'utf8');
     let passes = JSON.parse(buffer);
-    pass = passes.find(pass => pass.owner === owner);
+    pass = passes.find(pass => pass.owner === ownerId);
     return pass;
 }
 
-function addPass(passNumber, passOwner){
+function addPass(passNumber, ownerId, ownerName){
+    // Fetch pass data
     let buffer = fs.readFileSync(path.join(__dirname, 'passes.json'),'utf8');
     let passes = JSON.parse(buffer); 
 
+    // Fetch titles data
+    buffer = fs.readFileSync(path.join(__dirname, 'titles.json'), 'utf8');
+    let titleTypes = JSON.parse(buffer); 
+
     let newPass = {
         passNumber : passNumber,
-        owner : passOwner,
-        // Attributes are static to simualate fetching
+        ownerId : ownerId,
+        ownerName : ownerName,
+        // Attributes are random to simualate fetching
         // the transport title info from the pass number
-        title : 'Sub23',
-        paymentState : 'Em atraso',
+        titleType : randomItem(titleTypes).title,
+        paymentState : randomItem(PAYMENT_STATES),
         validity : '1-1-2040'
     };
 
+    // Check if the owner of the pass isn't
+    // already associated with another pass
     let i = passes.findIndex((value) =>{
         value.passNumber == passNumber
     });
-
-    // Check if the owner of the pass is
-    // already associated with another pass
     if (i == -1){
         // add a new pass
         passes.push(newPass);
@@ -36,6 +49,7 @@ function addPass(passNumber, passOwner){
         passes[i] = newPass;
     }
 
+    // Update files
     let updatedJSON = JSON.stringify(passes, null, 2);
     fs.writeFileSync(path.join(__dirname, 'passes.json'), updatedJSON);
 }
