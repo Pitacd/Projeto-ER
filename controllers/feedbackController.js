8635a03fd9b3a92bc10347aeaa08a7e8e400fa4d
Pitacd <jappita15@hotmail.com>
2023-12-19 21:42:30 +0000

    Add post f

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,5 +1,13 @@
+const {storageFeedback} = require('../models/feedback');
+
 function get(req, res){
-    res.render(`feedback.ejs`);
+    res.render('feedback.ejs', {pageTitle : "Feedback"});
+}
+function post(req,res){
+    let { about, classification, feedback } = req.body;
+    storageFeedback(about, classification, feedback);
+
+    res.redirect('/schedules');
 }
 
-module.exports = { get };
+module.exports = { get, post};
--- routes/FeedbackRoute.js
+++ routes/FeedbackRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const feedbackController = require('../controllers/feedbackController'); 
 
 router.get('/feedback', feedbackController.get);
+router.post('/feedback', feedbackController.post);
 
 module.exports = router;
